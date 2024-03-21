23bb3d12d56ac11e11152e38c66a59d1532ae126
Ilya Dybovsky <ilya77788899@mail.ru>
2024-03-21 19:05:38 +0300

    add login & registration pages no funcs for views in user

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
-
-
 from django.urls import path
+from user import views
 
+app_name = "user"
 
 urlpatterns = [
-    ...
+    path("registration/", views.registration, name="registration"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 
+def registration(request):
+    return render(request, "user/registration.html")
+
+
+def login(request):
+    return render(request, "user/login.html")
+
+
+def logout(request):
+    return redirect(reverse("main:index"))
